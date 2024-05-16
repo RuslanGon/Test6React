9f3effb967fb490c3ab91ae523159d0ce5abfd73
RuslanGon <ruslan441006@gmail.com>
2024-05-16 10:26:38 +0300

    wer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,20 @@
-import { combineReducers, createStore } from "redux";
-import { devToolsEnhancer } from "@redux-devtools/extension";
+// import { combineReducers} from "redux";
+// import { devToolsEnhancer } from "@redux-devtools/extension";
+
+import { configureStore } from "@reduxjs/toolkit";
 
 import { mailboxReduser } from "./mailbox/mailboxReducer";
 
-const rootReduser = combineReducers({
-    mailbox: mailboxReduser
-})
+// const rootReduser = combineReducers({
+//     mailbox: mailboxReduser
+// })
 
-const enhancer = devToolsEnhancer()
+// const enhancer = devToolsEnhancer()
+// export const store = createStore(rootReduser, enhancer)
 
-export const store = createStore(rootReduser, enhancer)
+
+export const store = configureStore({
+    reducer: {
+        mailbox: mailboxReduser
+    }
+  });
